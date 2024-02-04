a658a693bc6794532c6be1e808381ffecf9181de
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2024-02-04 04:36:26 +0000

    Fixed navigation for mobile

--- src/components/Hero/HeroCard.jsx
+++ src/components/Hero/HeroCard.jsx
@@ -6,7 +6,7 @@ function HeroCard() {
             <div className='card-item'>
                 <div className="hero-card">
                     <div>
-                        <img className="hero-image" src="../images/sunset.jpg" alt="Betsy" />
+                        <img className="hero-image" src="./images/sunset.jpg" alt="Betsy" />
                         <div className="card-overlay">
                             <div className="card-title-hero">Looking To Adopt?</div>
                             <a href="" className="button">Click Here</a>
@@ -18,7 +18,7 @@ function HeroCard() {
             <div className='card-item'>
                 <div className="hero-card">
                     <div>
-                        <img className="hero-image" src="../images/for-a-walk.jpg" alt="Betsy" />
+                        <img className="hero-image" src="./images/for-a-walk.jpg" alt="Betsy" />
                         <div className="card-overlay">
                             <div className="card-title-hero">Do you need someone to take in a pet you can no longer care for?</div>
                             <a href="" className="button">Click Here</a>
@@ -30,7 +30,7 @@ function HeroCard() {
             <div className='card-item'>
                 <div className="hero-card">
                     <div>
-                        <img className="hero-image" src="../images/baby-dog.jpg" alt="Betsy" />
+                        <img className="hero-image" src="./images/baby-dog.jpg" alt="Betsy" />
                         <div className="card-overlay">
                             <div className="card-title-hero">Are you looking for animal services - walkers, minders and nutritionists?</div>
                             <a href="" className="button">Click Here</a>
