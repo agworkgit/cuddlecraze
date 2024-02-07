ef258d412b5fc8798ad212d39d068247b1642a7d
Irene <irebazaga@gmail.com>
2024-02-07 22:27:42 +0000

    footer blog

--- src/components/AdviceCard/BlogPost.jsx
+++ src/components/AdviceCard/BlogPost.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './BlogPost.css';
 import './advice-card.css';
-
+import Footer from '../../components/Footer/Footer';
+import SectionBorder from '../../components/SectionBorder/SectionBorder';
 
 const BlogPost = ({ postData }) => {
   const [readStatus, setReadStatus] = useState({});
@@ -37,38 +38,43 @@ const BlogPost = ({ postData }) => {
   ));
 
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <div className="blog-post-content">
-          <div className="blog-post-image">
-            <img src={postData.image} alt={postData.title} />
-          </div>
-          <h2 className="blog-post-title">{postData.title}</h2>
-          <p className="blog-post-min-read">{postData.minutes} min read</p>
-          <p className="blog-post-description">{postData.description}</p>
-          <div className="blog-post-paragraphs">{contentParagraphs}</div>
-          <div className="blog-post-keywords">{keywordButtons}</div>
-          <div className="blog-post-buttons">
-            <button
-              className={`blog-post-read-button ${
-                readStatus[postData.id] ? 'read' : ''
-              }`}
-              onClick={toggleReadStatus}
-            >
-              {readStatus[postData.id] ? '✔️ Read!' : '📰 Mark as Read'}
-            </button>
-            <button
-              className={`blog-post-fav-button ${
-                favorites.includes(postData.id) ? 'fav' : ''
-              }`}
-              onClick={toggleFavorite}
-            >
-              {favorites.includes(postData.id) ? '💖 Added!' : '🤍 Add to favourites'}
-            </button>
+    <>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <div className="blog-post-content">
+            <div className="blog-post-image">
+              <img src={postData.image} alt={postData.title} />
+            </div>
+            <h2 className="blog-post-title">{postData.title}</h2>
+            <p className="blog-post-min-read">{postData.minutes} min read</p>
+            <h3 className="blog-post-description">{postData.description}</h3>
+            <div className="blog-post-paragraphs">{contentParagraphs}</div>
+            <div className="blog-post-keywords">{keywordButtons}</div>
+            <div className="blog-post-buttons">
+              <button
+                className={`blog-post-read-button ${
+                  readStatus[postData.id] ? 'read' : ''
+                }`}
+                onClick={toggleReadStatus}
+              >
+                {readStatus[postData.id] ? '✔️ Read!' : '📰 Mark as Read'}
+              </button>
+              <button
+                className={`blog-post-fav-button ${
+                  favorites.includes(postData.id) ? 'fav' : ''
+                }`}
+                onClick={toggleFavorite}
+              >
+                {favorites.includes(postData.id) ? '💖 Added!' : '🤍 Add to favourites'}
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <SectionBorder />
+      <Footer />
+      <SectionBorder />
+    </>
   );
 };
 
