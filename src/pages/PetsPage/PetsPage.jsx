0a41e288541ff32a96a89a150fa2811b9a559511
ABlam87 <adamblampied@gmail.com>
2024-02-08 20:44:54 +0000

    updated PetCard, PetsPage and populated the pet-data json file

--- src/pages/PetsPage/PetsPage.jsx
+++ src/pages/PetsPage/PetsPage.jsx
@@ -2,15 +2,25 @@ import './pets-page.css';
 import PetCard from '../../components/PetCard/PetCard';
 import Footer from '../../components/Footer/Footer';
 import SectionBorder from '../../components/SectionBorder/SectionBorder';
+import petData from '../../components/PetCard/data/petData.json';
 
 function PetsPage() {
+    const petsContent = $('#pets-content')
+    
     return (
         <div id="pets-page" className='pets-container'>
-            <div className='pets-content grid'>
-                <PetCard />
-                <PetCard />
-                <PetCard />
-                <PetCard />
+            <div id="pets-content" className='pets-content grid'>
+                {petData.map((petData) => (
+                    <PetCard 
+                    key={petData.id}
+                    name={petData.name}
+                    location={petData.location}
+                    age={petData.age}
+                    breed={petData.breed}
+                    image={petData.image}
+                    specialRequirements={petData.specialRequirements}
+                    description={petData.description} />
+                    ))}
             </div>
             <SectionBorder/>
             <Footer />
