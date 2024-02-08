656738ed10757a64bf1a2ba257d7a434ddb2901a
Irene <irebazaga@gmail.com>
2024-02-08 22:20:40 +0000

    my account - profile editable

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './account-page.css';
 import Footer from '../../components/Footer/Footer';
 import SectionBorder from '../../components/SectionBorder/SectionBorder';
@@ -6,14 +7,25 @@ import AccountFavourite from '../../components/AccountFavourite/AccountFavourite
 import AccountAdopted from '../../components/AccountAdopted/AccountAdopted';
 
 function AccountPage() {
+    const [activeSection, setActiveSection] = useState('account'); // 'account', 'favorites', 'adopted'
+
+    const handleSectionChange = (section) => {
+        setActiveSection(section);
+    };
+
     return (
         <div id="account-page" className='account-container'>
-            <div className='account-content grid'>
-            <AccountHead />
-            <AccountFavourite />
-            <AccountAdopted />
+            <div className="section-buttons">
+                <button className="btn-primary" onClick={() => handleSectionChange('account')}>My Account</button>
+                <button className="btn-primary" onClick={() => handleSectionChange('favorites')}>Favorites</button>
+                <button className="btn-primary" onClick={() => handleSectionChange('adopted')}>Adopted</button>
             </div>
-            <SectionBorder/>
+            <div className='account-content grid'>
+                {activeSection === 'account' && <AccountHead />}
+                {activeSection === 'favorites' && <AccountFavourite />}
+                {activeSection === 'adopted' && <AccountAdopted />}
+            </div>
+            <SectionBorder />
             <Footer />
         </div>
     );
