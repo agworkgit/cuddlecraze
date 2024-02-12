d766db84dfed268fd114feeff68406deac34631d
ABlam87 <adamblampied@gmail.com>
2024-02-12 16:47:05 +0000

    added ConnectionEmail component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import PetsPage from './pages/PetsPage/PetsPage';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
